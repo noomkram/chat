220b59486b7d55002e5ac3dd996e30e57ba8dfa0
Mark Moon <noomkram@yahoo.com>
2017-06-04 15:07:48 -0500

    scripts for setting up postgres in docker container

---
+++ chat-api/chat-api.sh
@@ -0,0 +1,3 @@
+./mvnw -P docker-db clean install docker:build
+docker create -p 5432:5432 -e POSTGRES_DB=ora-db -e POSTGRES_PASSWORD=password -e POSTGRES_USER=system --name ora-db ora-db
+docker start ora-db
