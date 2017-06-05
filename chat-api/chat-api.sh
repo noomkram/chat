ae3874ca1ac28c3ddbdc9c6f2a542f7d81265e06
Mark Moon <noomkram@yahoo.com>
2017-06-04 22:01:42 -0500

    added docker cleanup preamble

--- chat-api/chat-api.sh
+++ chat-api/chat-api.sh
@@ -1,3 +1,6 @@
+docker stop ora-db
+docker rm -v ora-db
+docker rmi ora-db
 ./mvnw -P docker-db clean install docker:build
 docker create -p 5432:5432 -e POSTGRES_DB=ora-db -e POSTGRES_PASSWORD=password -e POSTGRES_USER=system --name ora-db ora-db
 docker start ora-db
--- chat-ws/chat-ws.sh
+++ chat-ws/chat-ws.sh
@@ -1,7 +1,10 @@
 #
 # Assumes VM IP is 192.168.99.100
-# Add -e datasource.host=<host.ip> to the docker create
+# Add -e datasource.host=<host.ip> to the docker create if host is on different IP
 #
+docker stop ora-ws
+docker rm -v ora-ws
+docker rmi ora-ws
 ./mvnw clean install docker:build
 docker create -p 8080:8080 --name ora-ws ora-ws
 docker start ora-ws
